628b4cdac0db5cd0ac520a517ae9965462d98593
Andis Paudel <andispaudel@gmail.com>
2025-05-15 08:59:08 +0545

    dockerfile created and env fixes

--- noteit_/config.py
+++ noteit_/config.py
@@ -9,5 +9,15 @@ APP_NAME = os.getenv('APP_NAME', 'noteit')
 ENV = os.getenv('ENV', 'development')
 DEBUG = os.getenv('DEBUG', 'FALSE') == 'TRUE'
 SECRET_KEY = os.getenv('SECRET_KEY')
-DATABASE_URL = os.getenv('DATABASE_URL')
-PORT = int(os.getenv('PORT', 8000))
+
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT', '3306')
+DB_NAME = os.getenv('DB_NAME')
+
+#DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+print(DATABASE_URL)
+
+PORT = int(os.getenv("PORT", 8000))
